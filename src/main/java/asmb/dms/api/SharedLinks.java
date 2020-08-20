3b7ce5939314608af6765133cbfdae9dd4588c94
afrena <afrena@asmb.it>
2020-08-20 10:42:10 +0200

    add shared links

---
+++ src/main/java/asmb/dms/api/SharedLinks.java
@@ -0,0 +1,11 @@
+package asmb.dms.api;
+
+import asmb.dms.http.Session;
+
+public class SharedLinks extends Api {
+
+	public SharedLinks(Session session, String path) {
+		super(session, path);
+	}
+
+}
